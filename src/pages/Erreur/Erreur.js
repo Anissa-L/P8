79ae62309fa93fa5b75b7166067e93edb5fce279
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-10-02 21:23:33 +0200

    page erreur terminée

--- src/pages/Erreur/Erreur.js
+++ src/pages/Erreur/Erreur.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Erreur() {
   return (
-    <div>
-      <h1>Oups! La page que vous demandez n'existe pas.</h1>
+    <div className="erreur">
+      <div className="erreur-title">
+        <h2>404</h2>
+      </div>
+      <div className="erreur-info">
+        <h3>Oups! La page que vous demandez n'existe pas.</h3>
+      </div>
+      <NavLink to="/" className="erreur-link">
+        Retourner sur la page d’accueil
+      </NavLink>
     </div>
   );
 }
